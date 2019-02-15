85e0aa7c7b5fc41b5132f24b1b7c65dca60a3165
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-15 23:13:50 +0800

    still trying to understand undocumented 'previous hydra build' option

--- sinara-systems.nix
+++ sinara-systems.nix
@@ -5,23 +5,23 @@ let
   #artiq-board = import <mainChannel/artiq-board.nix> { inherit pkgs; };
   #conda-artiq-board = import <mainChannel/conda-artiq-board.nix> { inherit pkgs; };
 
-  target = "kasli";
+  #target = "kasli";
 
-  variants = ["berkeley" "mitll2" "mitll" "nudt" "sysu" "tsinghua2" "tsinghua" "unsw" "ustc" "wipm"];
-  jobs = pkgs.lib.lists.foldr (variant: start:
-    let
-      json = ../. + "/${variant}.json";
-      boardBinaries = artiq-board {
-        inherit target variant;
-        buildCommand = "python -m artiq.gateware.targets.kasli_generic ${json}";
-      };
-    in
-      start // {
-        "artiq-board-${target}-${variant}" = boardBinaries;
-        "conda-artiq-board-${target}-${variant}" = conda-artiq-board {
-          boardBinaries = boardBinaries;
-          inherit target variant;
-      };
-  }) {} variants;
+  #variants = ["berkeley" "mitll2" "mitll" "nudt" "sysu" "tsinghua2" "tsinghua" "unsw" "ustc" "wipm"];
+  #jobs = pkgs.lib.lists.foldr (variant: start:
+  #  let
+  #    json = ../. + "/${variant}.json";
+  #    boardBinaries = conda-artiq-board {
+  #      inherit target variant;
+  #      buildCommand = "python -m artiq.gateware.targets.kasli_generic ${json}";
+  #    };
+  #  in
+  #    start // {
+  #      "artiq-board-${target}-${variant}" = boardBinaries;
+  #      "conda-artiq-board-${target}-${variant}" = conda-artiq-board {
+  #        boardBinaries = boardBinaries;
+  #        inherit target variant;
+  #    };
+  #}) {} variants;
 in
   pkgs.runCommand "xxxxabcd" { } "echo ${mainBuild} > $out"
