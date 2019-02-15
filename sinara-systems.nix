e4f4584e2ab489e98d66ef4d4bf7cbf5f33eba57
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-15 23:17:44 +0800

    still trying to understand undocumented 'previous hydra build' option

--- sinara-systems.nix
+++ sinara-systems.nix
@@ -1,29 +1,5 @@
-{ pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import <nixpkgs> {}, mainBuild}:
 
-let
-  mainBuild = import <mainBuild>;
-  #artiq-board = import <mainChannel/artiq-board.nix> { inherit pkgs; };
-  #conda-artiq-board = import <mainChannel/conda-artiq-board.nix> { inherit pkgs; };
-
-  #target = "kasli";
-
-  #variants = ["berkeley" "mitll2" "mitll" "nudt" "sysu" "tsinghua2" "tsinghua" "unsw" "ustc" "wipm"];
-  #jobs = pkgs.lib.lists.foldr (variant: start:
-  #  let
-  #    json = ../. + "/${variant}.json";
-  #    boardBinaries = conda-artiq-board {
-  #      inherit target variant;
-  #      buildCommand = "python -m artiq.gateware.targets.kasli_generic ${json}";
-  #    };
-  #  in
-  #    start // {
-  #      "artiq-board-${target}-${variant}" = boardBinaries;
-  #      "conda-artiq-board-${target}-${variant}" = conda-artiq-board {
-  #        boardBinaries = boardBinaries;
-  #        inherit target variant;
-  #    };
-  #}) {} variants;
-in
-  {
-    foo = pkgs.runCommand "xxxxabcd" { } "echo ${mainBuild} > $out";
-  }
+{
+  foo = pkgs.runCommand "xxxxabcd" { } "echo ${mainBuild} > $out";
+}
