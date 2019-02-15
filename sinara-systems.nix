b265305f3edd8cbe02b1b95c72f52c49732f3486
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-15 23:50:44 +0800

    first theoretically working sinara-systems jobset attempt

--- sinara-systems.nix
+++ sinara-systems.nix
@@ -1,29 +1,27 @@
 { pkgs ? import <nixpkgs> {}}:
 
 let
-  mainChannel = import <mainChannel>;
-  #artiq-board = import <mainChannel/artiq-board.nix> { inherit pkgs; };
-  #conda-artiq-board = import <mainChannel/conda-artiq-board.nix> { inherit pkgs; };
+  artiq-board = import <mainChannel/artiq-board.nix> { inherit pkgs; };
+  conda-artiq-board = import <mainChannel/conda-artiq-board.nix> { inherit pkgs; };
 
-  #target = "kasli";
+  target = "kasli";
 
-  #variants = ["berkeley" "mitll2" "mitll" "nudt" "sysu" "tsinghua2" "tsinghua" "unsw" "ustc" "wipm"];
-  #jobs = pkgs.lib.lists.foldr (variant: start:
-  #  let
-  #    json = ../. + "/${variant}.json";
-  #    boardBinaries = conda-artiq-board {
-  #      inherit target variant;
-  #      buildCommand = "python -m artiq.gateware.targets.kasli_generic ${json}";
-  #    };
-  #  in
-  #    start // {
-  #      "artiq-board-${target}-${variant}" = boardBinaries;
-  #      "conda-artiq-board-${target}-${variant}" = conda-artiq-board {
-  #        boardBinaries = boardBinaries;
-  #        inherit target variant;
-  #    };
-  #}) {} variants;
+  variants = ["berkeley" "mitll2" "mitll" "nudt" "sysu" "tsinghua2" "tsinghua" "unsw" "ustc" "wipm"];
+  sinaraSystemsPkgs = pkgs.lib.lists.foldr (variant: start:
+    let
+      json = <sinaraSystemsSrc> + "/${variant}.json";
+      boardBinaries = conda-artiq-board {
+        inherit target variant;
+        buildCommand = "python -m artiq.gateware.targets.kasli_generic ${json}";
+      };
+    in
+      start // {
+        "artiq-board-${target}-${variant}" = boardBinaries;
+        "conda-artiq-board-${target}-${variant}" = conda-artiq-board {
+          boardBinaries = boardBinaries;
+          inherit target variant;
+      };
+     }) {} variants;
+  jobs = builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) sinaraSystemsPkgs;
 in
-  {
-    foo = pkgs.runCommand "xxxxabcd" { } "echo ${mainChannel} > $out";
-  }
+  jobs
