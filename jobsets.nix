2d2f9bfbf68599bdb65b5341628533ddb641a78f
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-17 12:31:27 +0800

    add sinara-systems jobset

--- jobsets.nix
+++ jobsets.nix
@@ -7,7 +7,7 @@
         "main": {
             "enabled": 1,
             "hidden": false,
-            "description": "js",
+            "description": "Main ARTIQ packages",
             "nixexprinput": "nixScripts",
             "nixexprpath": "main.nix",
             "checkinterval": 300,
@@ -16,9 +16,27 @@
             "emailoverride": "",
             "keepnr": 10,
             "inputs": {
-                "nixScripts": { "type": "git", "value": "git://github.com/m-labs/nix-scripts.git", "emailresponsible": false },
                 "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs-channels nixos-18.09", "emailresponsible": false },
+                "nixScripts": { "type": "git", "value": "git://github.com/m-labs/nix-scripts.git", "emailresponsible": false },
                 "artiqSrc": { "type": "git", "value": "git://github.com/m-labs/artiq.git master 1", "emailresponsible": false }
+            }
+        },
+        "sinara-systems": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "Board support artefacts for generic Sinara systems",
+            "nixexprinput": "nixScripts",
+            "nixexprpath": "sinara-systems.nix",
+            "checkinterval": 86400,
+            "schedulingshares": 1,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 10,
+            "inputs": {
+                "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs-channels nixos-18.09", "emailresponsible": false },
+                "nixScripts": { "type": "git", "value": "git://github.com/m-labs/nix-scripts.git", "emailresponsible": false },
+                "sinaraSystemsSrc": { "type": "git", "value": "git://github.com/m-labs/sinara-systems.git", "emailresponsible": false },
+                "m-labs": { "type": "sysbuild", "value": "artiq:main:generated-nix", "emailresponsible": false }
             }
         }
     }
