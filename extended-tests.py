8446f474ec831f76aa073dec18936b899d3a3728
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-18 23:53:48 +0800

    testing hydra log replacement hack

--- extended-tests.py
+++ extended-tests.py
@@ -1,3 +1,14 @@
-import os
+import json
+import bz2
+import sys
 
-os.system("artiq_flash -t kc705 -V nist_clock")
+with open(sys.argv[1], "r") as f:
+    hydra_json = json.load(f)
+
+drv = hydra_json["drvPath"]
+assert drv.startswith("/nix/store/")
+drv = drv[len("/nix/store/"):]
+hydra_log = "/var/lib/hydra/build-logs/" + drv[:2] + "/" + drv[2:]
+
+with bz2.BZ2File(hydra_log, "w") as f:
+    f.write(b"hack successful")
