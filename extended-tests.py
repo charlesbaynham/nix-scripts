e16c81e8da864cedc0ae7d390baa2e9746bd720f
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-19 00:01:20 +0800

    actually use bz2 log compression properly

--- extended-tests.py
+++ extended-tests.py
@@ -1,4 +1,5 @@
 import json
+import bz2
 import sys
 
 with open(sys.argv[1], "r") as f:
@@ -7,7 +8,7 @@ with open(sys.argv[1], "r") as f:
 drv = hydra_json["drvPath"]
 assert drv.startswith("/nix/store/")
 drv = drv[len("/nix/store/"):]
-hydra_log = "/var/lib/hydra/build-logs/" + drv[:2] + "/" + drv[2:]
+hydra_log = "/var/lib/hydra/build-logs/" + drv[:2] + "/" + drv[2:] + ".bz2"
 
-with open(hydra_log, "w") as f:
-    f.write("hack successful")
+with bz2.BZ2File(hydra_log, "w") as f:
+    f.write(b"hack successful")
