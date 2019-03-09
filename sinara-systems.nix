ebb00915fd1977392e7adf3a780aab9bd25e6471
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-03-09 12:37:06 +0800

    build wipm2

--- sinara-systems.nix
+++ sinara-systems.nix
@@ -13,7 +13,7 @@ let
 
     let
       target = "kasli";
-      variants = ["berkeley" "ist" "mitll2" "mitll" "nrc" "nudt" "sysu" "tsinghua2" "tsinghua" "ubirmingham" "unsw" "ustc" "wipm"];
+      variants = ["berkeley" "ist" "mitll2" "mitll" "nrc" "nudt" "sysu" "tsinghua2" "tsinghua" "ubirmingham" "unsw" "ustc" "wipm" "wipm2"];
 
       artiq = import <m-labs> { inherit pkgs; };
       artiq-board = import <m-labs/artiq-board.nix> { inherit pkgs; };
