c9003fb702ff4f3fabc94d754562ff2a39f09b55
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-03-14 17:51:50 +0800

    re-run kc705-tests

--- main.nix
+++ main.nix
@@ -20,7 +20,7 @@ let
   generateTestOkHash = pkgs.runCommand "generate-test-ok-hash" { buildInputs = [ pkgs.nix ]; }
     ''
     TMPDIR=`mktemp -d`
-    cp ${generatedNix}/pkgs/artiq-version.nix $TMPDIR/passed8
+    cp ${generatedNix}/pkgs/artiq-version.nix $TMPDIR/passed9
     HASH=`nix-hash --type sha256 --base32 $TMPDIR`
     echo \"$HASH\" > $out
     '';
@@ -73,7 +73,7 @@ in
       export ARTIQ_LOW_LATENCY=1
       python -m unittest discover -v artiq.test.coredevice
       mkdir $out
-      cp ${generatedNix}/pkgs/artiq-version.nix $out/passed8
+      cp ${generatedNix}/pkgs/artiq-version.nix $out/passed9
       '';
     };
   }
