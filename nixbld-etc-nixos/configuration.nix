48fb5026583d86facf3c81587195e3cf40260b59
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-18 19:48:58 +0800

    enable mosh

--- nixbld-etc-nixos/configuration.nix
+++ nixbld-etc-nixos/configuration.nix
@@ -44,6 +44,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.forwardX11 = true;
+  programs.mosh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 631 5901 80 443 ];
