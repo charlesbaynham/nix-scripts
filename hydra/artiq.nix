e412efa5d6c7f80e16fb8f15ffbeb1ef1c559fc4
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-18 22:30:22 +0800

    update nix-scripts git URL

--- hydra/artiq.nix
+++ hydra/artiq.nix
@@ -17,7 +17,7 @@
             "keepnr": 10,
             "inputs": {
                 "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs-channels nixos-19.03", "emailresponsible": false },
-                "nixScripts": { "type": "git", "value": "git://github.com/m-labs/nix-scripts.git", "emailresponsible": false },
+                "nixScripts": { "type": "git", "value": "https://git.m-labs.hk/M-Labs/nix-scripts.git", "emailresponsible": false },
                 "artiqSrc": { "type": "git", "value": "git://github.com/m-labs/artiq.git master 1", "emailresponsible": false }
             }
         },
@@ -34,7 +34,7 @@
             "keepnr": 10,
             "inputs": {
                 "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs-channels nixos-19.03", "emailresponsible": false },
-                "nixScripts": { "type": "git", "value": "git://github.com/m-labs/nix-scripts.git", "emailresponsible": false },
+                "nixScripts": { "type": "git", "value": "https://git.m-labs.hk/M-Labs/nix-scripts.git", "emailresponsible": false },
                 "sinaraSystemsSrc": { "type": "git", "value": "git://github.com/m-labs/sinara-systems.git master 1", "emailresponsible": false },
                 "m-labs": { "type": "sysbuild", "value": "artiq:main:generated-nix", "emailresponsible": false }
             }
