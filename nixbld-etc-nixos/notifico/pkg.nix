7cffd4f8f875d5d2f0c45cfe8d81ea7aa2ed49b3
Stephan Maka <stephan@spaceboyz.net>
2019-05-10 00:41:11 +0200

    add notifico/pkg

---
+++ nixbld-etc-nixos/notifico/pkg.nix
@@ -0,0 +1,132 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+#{ python2Packages, python2, fetchFromGitHub, fetchurl }:
+
+let
+  Flask-Gravatar = python2Packages.buildPythonPackage {
+    name = "Flask-Gravatar";
+    # src = fetchFromGitHub {
+    #   owner = "zzzsochi";
+    #   repo = "Flask-Gravatar";
+    #   rev = "317053bdd03e6d7bc339f5b2ba9b7cf722660d07";
+    #   sha256 = "0ckzfsgahl7iiizsn8ybvgmznwrkxf1x5iay1n302js8aii4zrhm";
+    # };
+    src = python2Packages.fetchPypi {
+      pname = "Flask-Gravatar";
+      version = "0.5.0";
+      sha256 = "1qb2ylirjajdqsmldhwfdhf8i86k7vlh3y4gnqfqj4n6q8qmyrk0";
+    };
+    propagatedBuildInputs = with python2Packages; [
+      pytestrunner
+      flask
+    ];
+    checkInputs = with python2Packages; [
+      check-manifest
+      coverage
+      isort
+      pydocstyle
+      pytestcache
+      pytestcov
+      pytestpep8
+      pytest
+      pygments
+    ];
+  };
+  # sniffer = python2Packages.buildPythonPackage {
+  #   name = "sniffer";
+  #   src = fetchFromGitHub {
+  #     owner = "jeffh";
+  #     repo = "sniffer";
+  #     rev = "v0.4.0";
+  #     sha256 = "19k5b585c7dwc5ml8g4aknd3za8bv8yh72p2p35a1jgdn09cl6dp";
+  #   };
+  #   propagatedBuildInputs = with python2Packages; [
+  #     colorama
+  #     termstyle
+  #     nose
+  #   ];
+  # };
+  utopia = python2Packages.buildPythonPackage {
+    name = "utopia";
+    src = fetchFromGitHub {
+      owner = "notifico";
+      repo = "utopia";
+      rev = "70293ed5e1ca55232e0fae71061e7e9b9b29be6f";
+      sha256 = "11cnh9l4d9jlhafnfis9si6kgk9zsdd5439qnhxh6dca3x4a986q";
+    };
+    propagatedBuildInputs = with python2Packages; [
+      gevent
+      blinker
+    ];
+    doCheck = false;
+    checkInputs = with python2Packages; [
+      # sniffer
+      nose
+    ];
+  };
+  Flask-WTF = python2Packages.flask_wtf.overrideAttrs(oa: rec {
+  	version = "0.8.4";
+  	src = python2Packages.fetchPypi {
+      pname = "Flask-WTF";
+      inherit version;
+      sha256 = "1khbwmlrcnk9f46f7kf531n06pkyfs6nc8fk273js9mj2igngg2y";
+    };
+  });
+  Flask-XML-RPC = python2Packages.flask_wtf.overrideAttrs(oa: rec {
+  	version = "0.1.2";
+  	src = python2Packages.fetchPypi {
+      pname = "Flask-XML-RPC";
+      inherit version;
+      sha256 = "1dwalj7pc5iid9l1k50q5mllirnn9f5s7jq54a66x48a4j179p2a";
+    };
+  });
+in
+  python2Packages.buildPythonApplication {
+    name = "notifico";
+    src = fetchFromGitHub {
+      owner = "notifico";
+      repo = "notifico";
+      rev = "6af849e4c75dff4d740051676f5a2093a44efcee";
+      sha256 = "18jifqdvjy4x5s1bh7vx501pin52g4n3hhw1z4m2c0h512z4spdr";
+    };
+    patches = [
+      (fetchurl {
+        url = https://github.com/whitequark/notifico/commit/22b582fad6cb97af6f7437e8462d720ddacc42ef.patch;
+        sha256 = "0w8i8hf1r8b0p1y1zn9vyvnyi20qp120aiyalqymhsxsh17mma52";
+      })
+    ];
+    propagatedBuildInputs = with python2Packages; [
+      flask
+      Flask-WTF
+      Flask-Gravatar
+      flask_sqlalchemy
+      Flask-XML-RPC
+      flask_mail
+      flask-caching
+      Fabric
+      sqlalchemy
+      utopia
+      gevent
+      oauth2
+      redis
+      gunicorn
+      requests
+      PyGithub
+      xmltodict
+      unidecode
+      raven
+      blinker
+      docopt
+      celery
+    ];
+    postInstall = ''
+      mkdir $out/bin
+      cat << EOF > $out/bin/notifico
+      #!${python2}/bin/python
+
+      import notifico
+      EOF
+      chmod +x $out/bin/notifico
+    '';
+  }
