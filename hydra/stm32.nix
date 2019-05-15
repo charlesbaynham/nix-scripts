e7d8ca8964496f5852df35276d49ab600ac8fcd7
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-05-15 10:39:10 +0800

    stm32: build Stabilizer firmware

--- hydra/stm32.nix
+++ hydra/stm32.nix
@@ -4,11 +4,11 @@
     ''
     cat > $out << EOF
     {
-        "adc2tcp": {
+        "stabilizer": {
             "enabled": 1,
             "hidden": false,
-            "description": "STM32 demo",
-            "nixexprinput": "adc2tcp",
+            "description": "Stabilizer firmware",
+            "nixexprinput": "stabilizer",
             "nixexprpath": "release.nix",
             "checkinterval": 300,
             "schedulingshares": 10,
@@ -17,7 +17,7 @@
             "keepnr": 10,
             "inputs": {
                 "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs-channels nixos-19.03", "emailresponsible": false },
-                "adc2tcp": { "type": "git", "value": "git://github.com/m-labs/adc2tcp.git", "emailresponsible": false },
+                "stabilizer": { "type": "git", "value": "git://github.com/quartiq/stabilizer.git", "emailresponsible": false },
                 "mozillaOverlay": { "type": "git", "value": "git://github.com/mozilla/nixpkgs-mozilla.git", "emailresponsible": false }
             }
         }
