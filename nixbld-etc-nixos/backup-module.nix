7882767a711de110e50aa4f1597710647f286580
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-05-25 00:31:32 +0800

    nixbld: back up more

--- nixbld-etc-nixos/backup-module.nix
+++ nixbld-etc-nixos/backup-module.nix
@@ -3,10 +3,24 @@ with lib;
 let
   makeBackup = pkgs.writeScript "make-backup" ''
     #!${pkgs.bash}/bin/bash
-    ${pkgs.gnutar}/bin/tar cf - /etc/nixos | \
+
+    set -e
+    umask 0077
+
+    DBDUMPDIR=`mktemp -d`
+    pushd $DBDUMPDIR
+
+    ${config.services.mysql.package}/bin/mysqldump --single-transaction flarum > flarum.sql
+    ${pkgs.sudo}/bin/sudo -u mattermost ${config.services.postgresql.package}/bin/pg_dump mattermost > mattermost.sql
+
+    ${pkgs.gnutar}/bin/tar cf - /etc/nixos /var/lib/gitea flarum.sql mattermost.sql | \
         ${pkgs.bzip2}/bin/bzip2 | \
         ${pkgs.gnupg}/bin/gpg --symmetric --batch --passphrase-file /etc/nixos/secret/backup-passphrase | \
         ${pkgs.rclone}/bin/rclone rcat --config /etc/nixos/secret/rclone.conf dropbox:backup-`date +%F`.tar.bz2.gpg
+
+    popd
+    rm -rf $DBDUMPDIR
+
     echo Backup done
   '';
   cfg = config.services.homu;
