87a93802e1262d44e72a3f879826c3c58a739e1d
Stephan Maka <stephan@spaceboyz.net>
2019-06-11 23:01:08 +0200

    extended-tests: fix ssh path

--- main.nix
+++ main.nix
@@ -96,7 +96,7 @@ in
       LOCKCTL=$(mktemp -d)
       mkfifo $LOCKCTL/lockctl
 
-      cat $LOCKCTL/lockctl | ssh rpi-1 'flock /tmp/board_lock-kc705-1 -c "echo Ok; cat"' | (
+      cat $LOCKCTL/lockctl | ${pkgs.openssh}/bin/ssh rpi-1 'flock /tmp/board_lock-kc705-1 -c "echo Ok; cat"' | (
         # End remote flock via FIFO
         atexit_unlock() {
           echo > $LOCKCTL/lockctl
