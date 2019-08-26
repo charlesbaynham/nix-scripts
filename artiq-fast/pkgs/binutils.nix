5e1d97618c4858f2ec0211a7974da4324cd50cd5
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-08-26 10:17:17 +0800

    binutils: consistent naming

--- artiq-fast/pkgs/binutils.nix
+++ artiq-fast/pkgs/binutils.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   basename = "binutils";
   inherit platform;
   version = "2.30";
-  name = "${basename}_${platform}-${version}";
+  name = "${basename}-${platform}-${version}";
   src = fetchurl {
     url = "https://ftp.gnu.org/gnu/binutils/binutils-${version}.tar.bz2";
     sha256 = "028cklfqaab24glva1ks2aqa1zxa6w6xmc8q34zs1sb7h22dxspg";
