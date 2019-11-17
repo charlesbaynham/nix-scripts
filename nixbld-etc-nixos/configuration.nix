3d8252acf8e99328eee9cab4c4723aff50e42bf1
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-11-17 09:33:31 +0800

    restore ARTIQ-3 manual

--- nixbld-etc-nixos/configuration.nix
+++ nixbld-etc-nixos/configuration.nix
@@ -445,6 +445,9 @@ ACTION=="add", SUBSYSTEM=="tty", \
         locations."/artiq/manual-release-4/" = {
           alias = "/var/www/m-labs.hk.old/artiq/manual-release-4/";
         };
+        locations."/artiq/manual-release-3/" = {
+          alias = "/var/www/m-labs.hk.old/artiq/manual-release-3/";
+        };
       };
     in {
       "m-labs.hk" = mainWebsite;
