9617de32f975e75fbc9684d9f70ebbdf988a4861
Stephan Maka <stephan@spaceboyz.net>
2020-04-16 02:59:56 +0200

    mattermost-github-integration: pin src rev

--- nixbld-etc-nixos/mattermost-github-integration/pkg.nix
+++ nixbld-etc-nixos/mattermost-github-integration/pkg.nix
@@ -7,7 +7,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "softdevteam";
     repo = "mattermost-github-integration";
-    rev = "master";
+    rev = "1124a0ff233b50ed6070cb84cfffd128ad219831";
     sha256 = "1hfvjaxjhliy8sv9j3616fkdwd2jqhfsj9ai7ggx88zhxknrfx85";
   };
   propagatedBuildInputs = [
