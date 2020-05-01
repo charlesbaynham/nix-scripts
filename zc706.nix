b1accc9810586a7c1cc3cc5d276462ab5537ff66
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-05-01 11:51:10 +0800

    zc706: trying to understand evaluation problems...

--- zc706.nix
+++ zc706.nix
@@ -1,6 +1,6 @@
 {
   pkgs ? import <nixpkgs> {},
-  artiq-zynq ? import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
+  artiq-zynq ? import <artiq-zynq/default.nix> { mozillaOverlay = import <mozillaOverlay>; },
 }:
   {
     test = artiq-zynq.zc706-sd-zip;
