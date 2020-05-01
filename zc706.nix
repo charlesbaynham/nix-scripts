1110d6fbb869dc7feb834c738143ea1fa0fbbaca
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-05-01 11:37:36 +0800

    zc706: trying to understand evaluation problems...

--- zc706.nix
+++ zc706.nix
@@ -2,4 +2,4 @@
   pkgs ? import <nixpkgs> {},
   artiq-zynq ? import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
 }:
-builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) artiq-zynq
+artiq-zynq
