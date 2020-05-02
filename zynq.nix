1af80bace345f94d006bb4615277f4e7d193e9cd
Stephan Maka <stephan@spaceboyz.net>
2020-05-03 01:49:57 +0200

    zynq: syntax

--- zynq.nix
+++ zynq.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> {},
-  artiq-zynq = import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
+  pkgs = import <nixpkgs> {};
+  artiq-zynq = import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; };
 in
 builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) artiq-zynq
