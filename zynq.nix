5a5c832de2aba03cf226f49a03c652b5049e716f
Stephan Maka <stephan@spaceboyz.net>
2020-05-03 01:45:54 +0200

    zynq: remove args

--- zynq.nix
+++ zynq.nix
@@ -1,8 +1,5 @@
-{
-  pkgs ? import <nixpkgs> {},
-  artiq-zynq ? import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
-}:
-builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) (
-  pkgs.lib.filterAttrs (key: value: builtins.substring 0 6 key == "zc706-")
-    artiq-zynq
-)
+let
+  pkgs = import <nixpkgs> {},
+  artiq-zynq = import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
+in
+builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) artiq-zynq
