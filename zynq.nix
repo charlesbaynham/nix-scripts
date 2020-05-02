1bfe9be05550c44ee6d853f8f3ee06cbaafb7d5f
Stephan Maka <stephan@spaceboyz.net>
2020-05-03 01:36:49 +0200

    zynq: try to filter for "zc706-" jobs

--- zynq.nix
+++ zynq.nix
@@ -2,4 +2,7 @@
   pkgs ? import <nixpkgs> {},
   artiq-zynq ? import <artiq-zynq> { mozillaOverlay = import <mozillaOverlay>; },
 }:
-builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) artiq-zynq
+builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) (
+  pkgs.lib.filterAttrs (key: value: builtins.substring 0 6 key == "zc706-")
+    artiq-zynq
+)
