107340b08ad99d65b3025551d69a96a853364238
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-06-15 14:03:25 +0800

    wfvm: add win-get

--- artiq-fast/wfvm/utils.nix
+++ artiq-fast/wfvm/utils.nix
@@ -51,10 +51,16 @@ rec {
     echo "SSH OK"
   '';
   win-put = pkgs.writeShellScriptBin "win-put" ''
-    echo scp windows $1 -\> $2
+    echo win-put $1 -\> $2
     ${pkgs.sshpass}/bin/sshpass -p1234 -- \
       ${pkgs.openssh}/bin/scp -P 2022 ${sshOpts} \
       $1 wfvm@localhost:$2
+  '';
+  win-get = pkgs.writeShellScriptBin "win-get" ''
+    echo win-get $1
+    ${pkgs.sshpass}/bin/sshpass -p1234 -- \
+      ${pkgs.openssh}/bin/scp -P 2022 ${sshOpts} \
+      wfvm@localhost:$1 .
   '';
 
   wfvm-run = { name, image, script, display ? false, isolateNetwork ? true, forwardedPorts ? [] }:
