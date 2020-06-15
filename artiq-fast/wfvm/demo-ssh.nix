5ab186e3dcc0d8af03c6f7a6799ca4168c487cee
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-06-15 20:09:29 +0800

    wfvm: allow network access in SSH demo

--- artiq-fast/wfvm/demo-ssh.nix
+++ artiq-fast/wfvm/demo-ssh.nix
@@ -6,6 +6,7 @@ in
   wfvm.utils.wfvm-run {
     name = "demo-ssh";
     image = import ./demo-image.nix { inherit pkgs; };
+    isolateNetwork = false;
     script = ''
       ${pkgs.sshpass}/bin/sshpass -p1234 -- ${pkgs.openssh}/bin/ssh -p 2022 wfvm@localhost -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
     '';
