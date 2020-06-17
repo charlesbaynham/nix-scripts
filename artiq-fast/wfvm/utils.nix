3d41477178db893888ef4e58e815970401aad95d
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-06-17 17:02:24 +0800

    wfvm: support transferring folders recursively

--- artiq-fast/wfvm/utils.nix
+++ artiq-fast/wfvm/utils.nix
@@ -53,13 +53,13 @@ rec {
   win-put = pkgs.writeShellScriptBin "win-put" ''
     echo win-put $1 -\> $2
     ${pkgs.sshpass}/bin/sshpass -p1234 -- \
-      ${pkgs.openssh}/bin/scp -P 2022 ${sshOpts} \
+      ${pkgs.openssh}/bin/scp -r -P 2022 ${sshOpts} \
       $1 wfvm@localhost:$2
   '';
   win-get = pkgs.writeShellScriptBin "win-get" ''
     echo win-get $1
     ${pkgs.sshpass}/bin/sshpass -p1234 -- \
-      ${pkgs.openssh}/bin/scp -T -P 2022 ${sshOpts} \
+      ${pkgs.openssh}/bin/scp -r -T -P 2022 ${sshOpts} \
       wfvm@localhost:$1 .
   '';
 
