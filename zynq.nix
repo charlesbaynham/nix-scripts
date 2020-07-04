563161860e56fa45c4e3c39b04b247ab8aa0ae13
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-07-04 17:27:42 +0800

    zynq: add HITL test (WIP)

--- zynq.nix
+++ zynq.nix
@@ -20,4 +20,30 @@ in
     ) zc706.zc706
   ) // (
     builtins.mapAttrs (key: value: pkgs.lib.hydraJob value) artiq-zynq
-  )
+  ) // {
+    zc706-hitl-tests = pkgs.lib.hydraJob (pkgs.stdenv.mkDerivation {
+      name = "zc706-hitl-tests";
+
+      # requires patched Nix
+      __networked = true;
+
+      buildInputs = [
+        pkgs.openssh pkgs.rsync
+      ];
+      phases = [ "buildPhase" ];
+
+      buildPhase =
+        ''
+        export HOME=`mktemp -d`
+        mkdir $HOME/.ssh
+        cp /opt/hydra_id_rsa $HOME/.ssh/id_rsa
+        cp /opt/hydra_id_rsa.pub $HOME/.ssh/id_rsa.pub
+        echo "rpi-4 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJu+HhQmQAclub9K/B3xUQ28lBg+MEdqSeBuQNZgPGKB" > $HOME/.ssh/known_hosts
+        chmod 600 $HOME/.ssh/id_rsa
+
+        ${<artiq-zynq>}/remote_run.sh -d ${artiq-zynq.zc706-simple-jtag}
+
+        touch $out
+        '';
+    });
+  }
