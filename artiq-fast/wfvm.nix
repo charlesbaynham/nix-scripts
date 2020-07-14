cc00cfc1a8bf16b6a7e25b2c8a1187b45c8efb3b
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-07-14 15:28:00 +0800

    wfvm: bump

--- artiq-fast/wfvm.nix
+++ artiq-fast/wfvm.nix
@@ -10,7 +10,7 @@ let
   };
   wfvm = pkgs.fetchgit {
     url = "https://git.m-labs.hk/M-Labs/wfvm.git";
-    rev = "304a102b61ae1649739129510bbfc2f162e069b7";
-    sha256 = "0ss7z5inp2fbrqjpp296iy04m8v3bwiajhwa7w5ijixva5v2mmg0";
+    rev = "4b497938ffd9fcddf84a3dbe2f01524395292adb";
+    sha256 = "0m3kdbbcskqc1lf8b5f7ccbll9b7vkl4r00kbyx3yjb2rs6cqvil";
   };
 in import "${wfvm}/wfvm" { pkgs = (import wfvm-pkgs {}); }
