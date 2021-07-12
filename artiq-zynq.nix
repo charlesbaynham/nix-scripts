dacc57af741d77176d993aa94fbdb2cb8ba3a066
Sebastien Bourdeauducq <sb@m-labs.hk>
2021-07-12 16:41:03 +0800

    zynq: install matching openocd version on rpi

--- artiq-zynq.nix
+++ artiq-zynq.nix
@@ -44,7 +44,9 @@ in
         export OPENOCD_ZYNQ=${artiq-zynq.zynq-rs}/openocd
         export SZL=${szlEnv}
         pushd ${<artiq-zynq>}
-        bash ${<artiq-zynq>}/remote_run.sh -h rpi-4 -o "-F /dev/null -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i /opt/hydra_id_rsa" -d ${artiq-zynq.zc706-nist_qc2-jtag}
+        export NIX_SSHOPTS="-F /dev/null -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i /opt/hydra_id_rsa"
+        ssh rpi-4 "nix-store -r ${<nixpkgs>} --add-root openocd-pkgs --indirect && nix-env -e openocd && nix-env -iA openocd -f openocd-pkgs && rm openocd-pkgs"
+        bash ${<artiq-zynq>}/remote_run.sh -h rpi-4 -o $NIX_SSHOPTS -d ${artiq-zynq.zc706-nist_qc2-jtag}
         popd
 
         echo Waiting for the firmware to boot...
